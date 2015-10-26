7ad43f166a255934ecdd1aebbe26dfac04bf6655
Wenbo L <wenbolovesnz@gmail.com>
2015-10-27 11:28:14 +1300

    ...

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-require('../css/bootstrap.css');
-require('../css/app.css');
-import {login} from "./login";
+let injectTapEventPlugin = require("react-tap-event-plugin");
 
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+import {login} from "./login";
 const React = require('react');
+const ReactDOM = require('react-dom');
+const Mui = require('material-ui');
+const RaisedButton = Mui.RaisedButton;
+const AppBar = Mui.AppBar;
 
 const App = React.createClass({
 	render(){
 		return <div>
-			<ItemList items = {[{text:'1'}, {text:'2'}]}/>
+			<ItemList items = {[{text:'1', key: '1'}, {text:'2', key:'2'}]}/>
 		</div>
 	}
 });
@@ -24,21 +33,18 @@ const ItemList = React.createClass({
 			</div>	
 		);
 	}
-})
+});
 
 const Item = React.createClass({
 	render(){
 		return(
 			<div>
-				{ this.props.text}									
+				<RaisedButton label={this.props.text} primary={true} />
 			</div>
 		)
 	}
 });
 
-React.render( <App/>, window.document.getElementById('target'));
-
-login('owen', 'goodman');
-
+ReactDOM.render( <App/>, window.document.getElementById('target'));
 
 console.log('React App Started.');
